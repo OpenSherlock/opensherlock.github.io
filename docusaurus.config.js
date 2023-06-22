fe2b628c99131cb09ce462ff56de3ac70930c9f2
Aaditya Bhatia <aadityabhatia@gmail.com>
2023-06-22 14:23:10 -0400

    docusaurus library

---
+++ docusaurus.config.js
@@ -0,0 +1,83 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'OpenSherlock',
+  tagline: 'A cognitive system that anticipates and learns',
+  favicon: 'img/cooperation.svg',
+
+  url: 'https://opensherlock.github.io',
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  organizationName: 'opensherlock',
+  projectName: 'opensherlock.github.io',
+  trailingSlash: true,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful metadata like html lang
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          editUrl:
+            'https://github.com/OpenSherlock/opensherlock.github.io',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/opensherlock-social-card.png',
+      navbar: {
+        title: 'OpenSherlock',
+        logo: {
+          alt: 'OpenSherlock',
+          src: 'img/cooperation.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'defaultSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            href: 'https://github.com/OpenSherlock',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} TopicQuests. All Rights Reserved.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
